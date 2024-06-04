f4ac6095485136aef8969264b12bc36ac3cd72ba
Aaron Echosit <126263606+donecho1@users.noreply.github.com>
2024-06-04 11:43:24 +0300

    create security group1

--- main.tf
+++ main.tf
@@ -13,12 +13,49 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
+data "aws_vpc" "default"{
+default =true
+}
+
+<<<<<<< HEAD
 
 resource "aws_instance" "blog" {
+=======
+resource "aws_instance" "blog" {
+>>>>>>> 2800e1b1cd304849cfbc1b020d532e1ec6db524c
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
+}
+
+resource "aws_security_group" "blog"{
+name        = "blog"
+description ="Allow http and https in. Allow everything out"
+
+vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group" "blog_https_in" {
+  type       = "ingress"
+  from-port  = 443
+  to_port    = 443
+  protocal   = "tcp"
+  cidr_block = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group" "blog_everything_out" {
+  type       = "egress"
+  from-port  = 0
+  to_port    = 0
+  protocal   = "-1"
+  cidr_block = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
